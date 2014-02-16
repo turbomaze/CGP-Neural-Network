081438836c914c827d6206efe0eca34d2e7b4160
Anthony Dee <bobhoward72@gmail.com>
2014-02-16 02:23:09 -0800

    Adds a 2 input -> 3 RGB output CGP Being plotter. One parent is displayed in addition to 5 mutated children.

--- js/cgp.js
+++ js/cgp.js
@@ -16,18 +16,74 @@
 
 /*********************
  * working variables */
+var canvas;
+var ctx;
 
 /******************
  * work functions */
 function initCGPNeuralNet() {
+	var start = currentTimeMillis();
+	/*
 	var handler = new Handler(1, 1+4); //1+4 strategy
 	for (var ai = 0; ai < 10; ai++) {
-		//handler.step();
+		handler.step();
 	}
+	*/
+
+	canvas = $('#canvas');
+	ctx = canvas.getContext('2d');
+	
+	////////////////////////////////////////////////////////////////////////////////////
+	//grid of randomly generated images (2 variable input CGPs -> 3 RGB outputs % 256)//
+	var border = 8;
+	var numcellsy = 2;
+	var numcellsx = 3;
+	var yunit = Math.floor((canvas.height-(numcellsy-1)*border)/numcellsy); //300
+	var xunit = Math.floor((canvas.width-(numcellsx-1)*border)/numcellsx); //400
+	var originalGenerator = new CGPBeing(2, 4, 3, 3);
+	var imageGenerator = originalGenerator;
+	for (var ai = 0; ai < numcellsy; ai++) {
+		for (var bi = 0; bi < numcellsx; bi++) {
+			var currImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+			for (var yi = ai*(yunit+border); yi < (ai)*(yunit+border)+yunit; yi++) {
+				for (var xi = bi*(xunit+border); xi < (bi)*(xunit+border)+xunit; xi++) {
+					var res = imageGenerator.evaluate(
+						[xi-bi*(xunit+border), yi-ai*(yunit+border)]
+					);
+					var idx = 4*(yi*canvas.width + xi);
+					currImageData.data[idx+0] = res[0]%256;
+					currImageData.data[idx+1] = res[1]%256;
+					currImageData.data[idx+2] = res[2]%256;
+					currImageData.data[idx+3] = 255;
+				}
+			}
+			ctx.putImageData(currImageData, 0, 0);
+			imageGenerator = originalGenerator.mutate();
+		}
+	}
+
+	console.log((currentTimeMillis() - start) + 'ms');
+}
+
+function drawPoint(pos, r, color) {
+	ctx.fillStyle = color || 'rgba(255, 255, 255, 0.3)';
+	ctx.beginPath();
+	ctx.arc(pos.x, pos.y, r, 0, 2*Math.PI, true);
+	ctx.closePath();
+	ctx.fill();
 }
 
 /********************
  * helper functions */
+function $(sel) {
+	if (sel.charAt(0) === '#') return document.getElementById(sel.substring(1));
+	else return false;
+}
+
+function currentTimeMillis() {
+	return new Date().getTime();
+}
+
 function getRandNum(lower, upper) { //returns number in [lower, upper)
 	return Math.floor((Math.random()*(upper-lower))+lower);
 }
@@ -133,7 +189,41 @@ function CGPBeing(numInputs, cols, rows, numOutputs) {
 	};
 
 	this.mutate = function() {
-		return new CGPBeing(this.numInputs, this.cols, this.rows, this.numOutputs);
+		var ret = new CGPBeing(this.numInputs, this.cols, this.rows, this.numOutputs);
+		var mutatedGenes = [];
+		var numGenes = this.genes.length;
+		var mutRate = 0.10;
+		for (var ai = 0; ai < numGenes-this.numOutputs; ai++) {
+			var currentGene = [];
+			for (var bi = 0; bi < 3; bi++) {
+				if (Math.random() < mutRate) {
+					var col = ai/this.numRows;
+					switch (bi) {
+						case 0:
+							currentGene.push(getRandFunctionId());
+							break;
+						default:
+							currentGene.push(
+								getRandInputId(col, this.numInputs, this.numRows)
+							);
+							break;
+					}
+				} else {
+					currentGene.push(this.genes[ai][bi]);
+				}
+			}
+			mutatedGenes.push(currentGene);
+		}
+		for (var ai = numGenes-this.numOutputs; ai < numGenes; ai++) {
+			if (Math.random() < mutRate) {
+				mutatedGenes.push(getRandInputId(this.cols, this.numInputs, this.rows));
+			} else {
+				mutatedGenes.push(this.genes[ai]);
+			}
+		}
+
+		ret.genes = mutatedGenes;
+		return ret;
 	};
 
 	this.squish = function() {
--- js/cgp_functions.js
+++ js/cgp_functions.js
@@ -1,6 +1,6 @@
 
 var cgpFunc = function(idx, params) {
-	var numFunctions = 6;
+	var numFunctions = 10;
 	switch (idx%numFunctions) {
 		case 0: return params[0]; //first param function
 		case 1: return params[1]; //second param function
@@ -8,6 +8,10 @@ var cgpFunc = function(idx, params) {
 		case 3: return params[0]-params[1]; //subtraction
 		case 4: return params[0]*params[1]; //multiplication
 		case 5: return params[1] === 0 ? 0 : params[0]/params[1]; //safe division (/0 -> 0)
+		case 6: return Math.pow(params[0], params[1]); //exponentiation
+		case 7: return params[0]*Math.sin(params[1]); //sin
+		case 8: return params[0]*Math.cos(params[1]); //cos
+		case 9: return params[0]*Math.tan(params[1]); //tan
 		default: return 0; //zero function
 	}
 };
