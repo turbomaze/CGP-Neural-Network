ad11f313513bf9c4f3b177a953000d9aeaf3e22b
Anthony Dee <bobhoward72@gmail.com>
2014-02-16 01:10:53 -0800

    Refines skeleton and defines the basic CGP Being.

--- js/cgp.js
+++ js/cgp.js
@@ -20,7 +20,10 @@
 /******************
  * work functions */
 function initCGPNeuralNet() {
-	
+	var handler = new Handler(1, 1+4); //1+4 strategy
+	for (var ai = 0; ai < 10; ai++) {
+		//handler.step();
+	}
 }
 
 /********************
@@ -31,99 +34,136 @@ function getRandNum(lower, upper) { //returns number in [lower, upper)
 
 /***********
  * objects */
-function Handler(numIslands) {
+function Handler(numIslands, popSize) {
 	this.islands = [];
-	for (var ai = 0; ai < numIslands; ai++) this.islands.push(new World());
+	this.popSize = popSize;
+	for (var ai = 0; ai < numIslands; ai++) this.islands.push(new Island(popSize));
 
 	this.step = function() {
+		//////////////////////////
+		//evolve all the islands//
+		var currentBests = [];
 		for (var ai = 0; ai < this.islands.length; ai++) {
-			this.islands[ai].evolve();
+			//evolve and get the current best solution and its score
+			currentBests.push(this.islands[ai].evolve()); 
 		}
+		var currentBest = [Math.pow(2, 2000), ''];
+		for (var ai = 0; ai < this.num_islands; ai++) {
+			if (currentBests[ai][0] < currentBest[0]) {
+				currentBest = currentBests[ai];
+			}
+		}
+
+		/////////////
+		//reporting//
+		console.log('Generation #' + this.current_gen + '\n' + 
+					'Score: ' + currentBest[0] + '\n' +
+		/*			'Duration: ' + duration + 'ms, ' + 
+						'Avg: ' + avgDuration + 'ms, ' + 
+						'Total: ' + this.time_elapsed + 'ms\n' +
+		*/			'Solution: ' + currentBest[1] + '\n'
+		);
 	};
 }
 
-function Island(pop_size) {
-	this.pop_size = pop_size;
-	this.population = [new CGPBeing()];
+function Island(popSize) {
+	this.popSize = popSize;
+	this.population = [new CGPBeing(2, 2, 2, 1)];
 
 	//////////////////
 	//work functions//
 	this.evolve = function() {
-		this.assess();
-
+		var selection = this.choose();
 		var newPopulation = [];
-		newPopulation.push(this.population[0]); //elitism, idx=0 is the prev pop's best
-		for (var ai = 1; ai < this.pop_size; ai++) {
-			newPopulation.push(this.population[0].mutate());
+		newPopulation.push(selection);
+		for (var ai = 1; ai < this.popSize; ai++) {
+			newPopulation.push(selection.mutate());
 		}
 
 		this.population = newPopulation;
+		return [selection.getFitness(), selection.squish()];
 	};
 
-	this.assess = function() {
-		var scores = [];
-		for (var ai = 0; ai < this.population.length; ai++) {
+	this.choose = function() {
+		var scores = [this.population[0].getFitness()];
+		var selection = this.population[0];
+		for (var ai = 1; ai < this.population.length; ai++) {
 			scores[ai] = this.population[ai].getFitness();
-		}
-		this.correspQuickSort(scores, this.population, 0, this.population.length-1);
-	};
-
-	this.correspQuickSort = function(v, s, low, high) {
-		var pivot;
-		if (high > low) {
-			pivot = this.correspPartition(v, s, low, high);
-			this.correspQuickSort(v, s, low, pivot-1);
-			this.correspQuickSort(v, s, pivot+1, high);
-		}
-	};
-	
-	this.correspPartition = function(v, s, low, high) {
-		var pivotVal = v[low];
-		var correspPivotVal = s[low];
-		var pivotIndex = low;
-		var left = low;			
-		var right = high;
-		//console.log('2bii1');
-		while (left < right) {
-			//console.log('l: ' + left + ', r: ' + right);
-			while (v[left] <= pivotVal) {
-				//console.log('in');
-				if (left+1 < v.length) {
-					left += 1;
-				} else {
-					break;
-				}
-			}
-			while (v[right] > pivotVal) {
-				if (right > 0) {
-					right -= 1;
-				} else {
-					break;
-				}
-			}
-			if (left < right) {
-				////////////////////////
-				//swaps left and right//
-				var temp_d = v[left];
-				var temp_B = s[left];
-				v[left] = v[right];
-				s[left] = s[right];
-				v[right] = temp_d;		
-				s[right] = temp_B;
+			if (scores[ai] >= scores[0]) { //the "or equals" in >= is very important!
+				selection = this.population[ai];
 			}
 		}
-		//console.log('2bii2');
-		v[low] = v[right];
-		s[low] = s[right];
-		v[right] = pivotVal;
-		s[right] = correspPivotVal;
-		
-		return right;
+		return selection;
 	};
 }
 
-function CGPBeing() {
-	this.genes = [];
+function CGPBeing(numInputs, cols, rows, numOutputs) {
+	this.numInputs = numInputs;
+	this.cols = cols;
+	this.rows = rows;
+	this.numOutputs = numOutputs;
+	this.genes = getRandBeing(this.numInputs, this.cols, this.rows, this.numOutputs);
+
+	this.getFitness = function() {
+
+	};
+
+	this.evaluate = function(inputs) {
+		var values = [];
+		for (var ai = 0; ai < this.numInputs; ai++) values.push(inputs[ai]);
+
+		for (var ai = 0; ai < this.cols; ai++) { //for each column
+			for (var bi = 0; bi < this.rows; bi++) { //each row in that column
+				var idx = ai*this.rows + bi;
+				values.push(
+					cgpFunc(
+						this.genes[idx][0], //function id
+						[values[this.genes[idx][1]], values[this.genes[idx][2]]] //inputs
+					)
+				); //evaluate the node at (col, row)
+			}
+		}
+
+		var ret = [];
+		var numGenes = this.genes.length;
+		for (var ai = numGenes-this.numOutputs; ai < numGenes; ai++) {
+			ret.push(values[this.genes[ai]]); //gather up all the outputs
+		}
+		return ret;
+	};
+
+	this.mutate = function() {
+		return new CGPBeing(this.numInputs, this.cols, this.rows, this.numOutputs);
+	};
+
+	this.squish = function() {
+		//btoa([1,2,0.2145566]) = "MSwyLDAuMjE0NTU2Ng=="
+		//atob("MSwyLDAuMjE0NTU2Ng==").split(",").map(parseFloat) = [1,2,0.2145566]
+		return btoa(this.genes);
+	};
+
+	function getRandFunctionId() { 
+		return getRandNum(0, 256); 
+	}
+
+	function getRandInputId(col, numInputs, rows) { 
+		return getRandNum(0, col*rows+numInputs);
+	}
+
+	function getRandBeing(numInputs, cols, rows, numOutputs) {
+		var ret = [];
+		for (var ai = 0; ai < cols; ai++) { //for each column
+			for (var bi = 0; bi < rows; bi++) { //each row in that column
+				ret.push([getRandFunctionId(),
+						  getRandInputId(ai, numInputs, rows), 
+						  getRandInputId(ai, numInputs, rows)]); //add genes
+			}
+		}
+		for (var ai = 0; ai < numOutputs; ai++) { //assign the output nodes
+			ret.push(getRandInputId(cols, numInputs, rows));
+		}
+		return ret;
+	}
 }
 
 window.addEventListener('load', function() {
--- js/cgp_functions.js
+++ js/cgp_functions.js
@@ -1,7 +1,13 @@
 
 var cgpFunc = function(idx, params) {
-	switch (idx) {
-		default:
-			return params[0];
+	var numFunctions = 6;
+	switch (idx%numFunctions) {
+		case 0: return params[0]; //first param function
+		case 1: return params[1]; //second param function
+		case 2: return params[0]+params[1]; //addition
+		case 3: return params[0]-params[1]; //subtraction
+		case 4: return params[0]*params[1]; //multiplication
+		case 5: return params[1] === 0 ? 0 : params[0]/params[1]; //safe division (/0 -> 0)
+		default: return 0; //zero function
 	}
 };
